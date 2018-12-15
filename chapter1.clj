478e993686699baef30154ccf174ba8c2bc039ca
Iwo Herka <hi@iwoherka.eu>
2018-12-15 15:44:14 +0100

    Add exercise 1.7

--- chapter1.clj
+++ chapter1.clj
@@ -19,3 +19,34 @@
 
 (defn sum-of-squares-of-larger-two [a b c]
   (apply sum-of-squares (larger-two a b c)))
+
+
+; Exercise 1.7 The good-enough? test used in computing
+; square roots will not be very effective for finding
+; the square roots of very small number. Also, in real
+; computers, arithmetic operations are almost always
+; performed with limited precision. This makes our test
+; inadequate for very large numbers. [...] An alternative
+; strategy for implementing good-enough? is to watch how
+; guess changes from one iteration to the next and to
+; stop when the change is very small fraction of the guess.
+; Design a square-root procedure that uses this kind of
+; end test.
+
+(defn abs [n]
+  (if (pos? n) n (- 0 n)))
+
+(defn improve [g n]
+  (/ (+ g (/ n g)) 2))
+
+(defn sqrt-iter [n guess E]
+  (let [guess' (improve guess n)
+        delta (abs (- guess guess'))]
+      (if (< delta E)
+        guess
+        (sqrt-iter n guess' E))))
+
+(defn sqrt [n E]
+  "Calculate square root of n using Newton's method,
+   where E is the desired precision."
+  (sqrt-iter n (improve n n) E))
